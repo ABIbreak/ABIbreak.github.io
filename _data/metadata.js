d72357b458a4a774599fefd979d5705c925c5a73
George Chen <code@georgechen.me>
2026-05-19 16:42:17 -0500

    Initial commit

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+export default {
+	title: "George Chen",
+	url: "https://georgechen.me/",
+	language: "en",
+	description: "",
+	author: {
+		name: "George Chen",
+		email: "hello[at]georgechen[dot]me",
+		url: "https://georgechen.me/"
+	}
+}
---
+++ content/blog/hello_world.md
@@ -0,0 +1,6 @@
+---
+title: Hello World!
+---
+
+This site is based off of the [eleventy blog template](https://github.com/11ty/eleventy-base-blog).
+
